072c34447543a0fa3a3093a385795ea1bb570874
tedduvarunsandesh-34 <tedduvarun376@gmail.com>
2025-11-14 22:22:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "736532225-w") 
+    API_ID    = os.environ.get("API_ID", "28408609")
+    API_HASH  = os.environ.get("API_HASH", "d6ddeafb0c189d91b8197ad49103e806")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8332453604:AAGM_C2IlmK6bp7n9pNRvmfrUuRMDBjP_Eo") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","Yato")     
-    DB_URL  = os.environ.get("DB_URL","mongodb")
+    DB_NAME = os.environ.get("DB_NAME","Govar")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://teddugovardhan544_db_user:WVjIA96jQ31net0j@cluster0.kwkkleo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     PORT = os.environ.get("PORT", "8080")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
-    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1001868871195"))
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5665480584').split()]
+    FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'All_animes_in_teluguu_vs').split(',')
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002599753693"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002599753693"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
